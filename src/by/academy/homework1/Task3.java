e292a5a8ed6ecab4cddff38807fefe6c7b5a47b9
Artem <118839457+ArtemButsko@users.noreply.github.com>
2022-11-30 20:16:13 +0300

    - m first commit

--- src/by/academy/homework1/Task3.java
+++ src/by/academy/homework1/Task3.java
@@ -12,7 +12,7 @@ public class Task3 {
 
         if (i > 0 & i < 10) {
             System.out.println("Таблица умножения для " + i + ": ");
-            for (int j = 1; j <= 9; j++) {
+            for (int j = 1; j <= 10; j++) {
                 int a = i * j;
                 System.out.println("" + i + " * " + j + " = " + a);
             }
