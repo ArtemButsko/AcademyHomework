f3b551fc8af3a4706a5841c70757dba83a4a4ba4
Artem <118839457+ArtemButsko@users.noreply.github.com>
2022-11-30 20:10:21 +0300

    - m first commit

--- src/by/academy/homework1/Task1.java
+++ src/by/academy/homework1/Task1.java
@@ -1,2 +1,33 @@
-package by.academy.homework1;public class Task1 {
+package by.academy.homework1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сумму покупки: ");
+        double sum = sc.nextDouble();
+        System.out.println("Введите возраст: ");
+        int age = sc.nextInt();
+        double finalSum = 0;
+
+        if (sum < 100) {
+            finalSum = sum - sum * 5 / 100;
+        } else if (sum >= 100 & sum < 200) {
+            finalSum = sum - sum * 7 / 100;
+
+        } else if (sum >= 200 & sum < 300) {
+
+            if (age > 18) {
+                finalSum = sum - sum * 16 / 100;
+            } else finalSum = sum - sum * 3 / 100;
+
+        } else if (sum >= 400) {
+            finalSum = sum - sum * 20 / 100;
+
+        }
+        sc.close();
+        System.out.println("Финальная сумма с учетом скидки = "+ finalSum);
+    }
 }
--- src/by/academy/homework1/Task4.java
+++ src/by/academy/homework1/Task4.java
@@ -1,2 +1,19 @@
-package by.academy.homework1;public class Task4 {
+package by.academy.homework1;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int j = 2;
+        int i = 0;
+
+        while (i < 1_000_000) {
+
+            i = (int) Math.pow(2, j);
+            j++;
+
+            System.out.print(i + " ");
+        }
+
+
+    }
 }
