ab3e70f4632a32ab25572c50a071b3b6d818ea40
Artem <118839457+ArtemButsko@users.noreply.github.com>
2023-02-08 22:30:28 +0300

    - m first commit

---
+++ src/by/academy/homework7/Task1/App.java
@@ -0,0 +1,23 @@
+package by.academy.homework7.Task1;
+
+
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class App {
+    private static final Random rand = new Random();
+
+    public static void main(String[] args) {
+
+
+        Map<Double, String> map = Stream.generate(() -> new Random().nextLong(100L)).limit(100)
+                .map(i -> i * Math.PI - 20).filter(i -> i < 100).sorted().skip(3)
+                .collect(Collectors.toMap(i -> i, v -> "Number: " + v, (first, second) -> first));
+
+        System.out.println(map);
+
+    }
+}
